907dc91d42862f3c0628dc870b95eed9bdfb77a8
Mulyaningsih <56246353+Mulyaningsih@users.noreply.github.com>
2020-10-26 21:28:15 +0700

    Add files via upload

---
+++ Praktikum/Praktikum 1/kenalan.php
@@ -0,0 +1,10 @@
+<?php
+class kenalan extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Oktavia Dwi Mulyaningsih<br>
+                saya tinggal di Banjarharjo<br>
+                saya suka memasak";
+    }
+
+}
---
+++ Praktikum/Praktikum 4/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Oktavia Dwi</li>
+        <li>Nama Belakang : Mulyaningsih</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Desa Kertasari Kecamatan Banjarharjo</li> </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Brebes</li></ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Memasak</li>
+        <li>Badminton</li>
+    </ul>
+</section>
